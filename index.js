25c752e2eb3035fe684f3e0af928cd2ffdc19b26
Juraci Vieira <juraci.vieira@gmail.com>
2017-03-10 13:08:57 -0300

    identifying the board

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const port = process.env.PORT || 3000;
+const urlencoded = bodyParser.urlencoded({ extended: true });
+let count = 0;
 
-app.post('/motion', (req, res) => {
-  console.log('motion detected');
+app.post('/motion', urlencoded, (req, res) => {
+  count++;
+  console.log(`Motion counter: ${count} from sensor: ${req.body.boardId}`);
   res.sendStatus(201);
 });
 
